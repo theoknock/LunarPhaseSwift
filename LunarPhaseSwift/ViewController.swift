4b336731729cb428d23f7c07613577d72a96c591
James Bush <theoknock@icloud.com>
2019-02-12 08:09:10 -0500

    Initial Commit

---
+++ LunarPhaseSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LunarPhaseSwift
+//
+//  Created by Xcode Developer on 2/12/19.
+//  Copyright © 2019 The Life of a Demoniac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
